f9e67ec18daef23c6bd258a0f04028a8e7427182
TIEmerald <niyejunze.j@gmail.com>
2017-12-14 17:55:05 +1100

    Added a Debug Tool to support all necessary debug methods...

---
+++ KNVUNDBaseDevelopPackage/Classes/Tools/KNVUNDDebugTool.h
@@ -0,0 +1,15 @@
+//
+//  KNVUNDDebugTool.h
+//  KNVUNDBaseDevelopPackage
+//
+//  Created by Erjian Ni on 14/12/17.
+//
+
+#import "KNVUNDBaseModel.h"
+
+@interface KNVUNDDebugTool : KNVUNDBaseModel
+
+#pragma mark - Method Related
++ (void)logSupportedMethodNameFromObject:(id)object;
+
+@end
---
+++ KNVUNDBaseDevelopPackage/Classes/Tools/KNVUNDDebugTool.m
@@ -0,0 +1,39 @@
+//
+//  KNVUNDDebugTool.m
+//  KNVUNDBaseDevelopPackage
+//
+//  Created by Erjian Ni on 14/12/17.
+//
+
+#import "KNVUNDDebugTool.h"
+
+// Libs
+#import <objc/runtime.h>
+
+@implementation KNVUNDDebugTool
+
+#pragma mark - KNVUNDBaseModle
+#pragma mark - Class Methods
+// You have to override this method if you want to show any log inside Class Method.
++ (BOOL)shouldShowClassMethodLog
+{
+    return YES;
+}
+
+#pragma mark - Method Related
++ (void)logSupportedMethodNameFromObject:(id)object
+{
+    int i=0;
+    unsigned int mc = 0;
+    Method * mlist = class_copyMethodList(object_getClass(object), &mc);
+    [self performConsoleLogWithLogStringFormat:@"Object %@ has %d supported methods",
+     [object description],
+     mc];
+    for(i = 0 ; i < mc ; i++){
+        [self performConsoleLogWithLogStringFormat:@"Method no #%d: %s",
+         i,
+         sel_getName(method_getName(mlist[i]))];
+    }
+}
+
+@end
