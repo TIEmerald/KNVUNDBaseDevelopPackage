7f109a39bbd9ecf9e181e864dd7dc6044cca2422
TIEmerald <niyejunze.j@gmail.com>
2018-01-31 19:05:13 +1100

    Add Methods to combine multiple Image into one whole image

--- KNVUNDBaseDevelopPackage/Classes/Tools/Theme Related/KNVUNDImageRelatedTool+ImageModifyRelated.h
+++ KNVUNDBaseDevelopPackage/Classes/Tools/Theme Related/KNVUNDImageRelatedTool+ImageModifyRelated.h
@@ -7,6 +7,11 @@
 
 #import "KNVUNDImageRelatedTool.h"
 
+typedef enum : NSUInteger {
+    KNVUNDImageRelatedTool_CombiningOrientation_Horizontal,
+    KNVUNDImageRelatedTool_CombiningOrientation_Vertical
+} KNVUNDImageRelatedTool_CombiningOrientation;
+
 @interface KNVUNDImageRelatedTool (ImageModifyRelated)
 
 #pragma mark - Modify Images
@@ -18,5 +23,10 @@
 // Reference: https://stackoverflow.com/questions/14857728/how-to-rotate-uiimage
 + (UIImage *_Nullable)getImageFromImage:(UIImage *_Nonnull)sourceImage withOrientation:(UIImageOrientation)orientation;
 + (UIImage *_Nullable)getImageFromImage:(UIImage *_Nonnull)sourceImage withRotatedDegree:(CGFloat)degrees;
+
+#pragma mark - Combining
++ (UIImage *_Nullable)getImageFromCombiningImages:(NSArray *_Nonnull)fromImages withOrientation:(KNVUNDImageRelatedTool_CombiningOrientation)orientation;
+/// defaultBackgroundColor --- the color we will fill in the canvas, By default is white
++ (UIImage *_Nullable)getImageFromCombiningImages:(NSArray *_Nonnull)fromImages withOrientation:(KNVUNDImageRelatedTool_CombiningOrientation)orientation withBackGroundColor:(UIColor *_Nullable)defaultBackgroundColor;
 
 @end
