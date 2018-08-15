63bc68136e9a6d0a8b5b64a1cbbd70e9bcb714b5
TIEmerald <niyejunze.j@gmail.com>
2018-08-15 12:17:59 +1000

    Update Log Function... Now we could log file into Cached Folder.

--- KNVUNDBaseDevelopPackage/Classes/Base Elements/Categories/NSObject/NSObject+KNVUNDLogRelated.m
+++ KNVUNDBaseDevelopPackage/Classes/Base Elements/Categories/NSObject/NSObject+KNVUNDLogRelated.m
@@ -23,6 +23,11 @@
     return YES;
 }
 
++ (BOOL)isLogIntoTempDirectory
+{
+    return NO;
+}
+
 + (void)performServerLogWithLogString:(NSString *)string
 {
     
@@ -31,9 +36,8 @@
 #pragma mark Log Related
 + (void)performConsoleLogWithLogString:(NSString *_Nonnull)string
 {
-    if ([self isDevelopMode] && [self shouldShowClassMethodLog]) {
-        NSLog(@"%@", string);
-    }
+    [self performConsoleLogWithLogString:string
+                      withControlBoolean:[self shouldShowClassMethodLog]];
 }
 
 + (void)performConsoleLogWithLogStringFormat:(NSString *_Nonnull)format, ... NS_FORMAT_FUNCTION(1,2)
@@ -64,12 +68,50 @@
 }
 
 #pragma mark - Support Methods
++ (void)performConsoleLogWithLogString:(NSString *_Nonnull)string withControlBoolean:(BOOL)couldLog
+{
+    if ([self isDevelopMode] && couldLog) {
+        NSLog(@"%@", string);
+        if ([self isLogIntoTempDirectory]) {
+            [self logStringIntoTempDirectory:string];
+        }
+    }
+}
+
 + (NSString *)getFormatedStringFromString:(NSString *)fromString
 {
     return [NSString stringWithFormat:@"[%@] %@",
             NSStringFromClass(self),
             fromString];
 }
+
++ (void)logStringIntoTempDirectory:(NSString *)string {
+    
+    // Setup date stuff
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"YYYY-dd-MM"];
+    NSDate *date = [NSDate date];
+    
+    // Paths - We're saving the data based on the day.
+    NSError *error;
+    NSString *folderPath = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0] stringByAppendingPathComponent:@"/KNVUNDLogFiles"];
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:folderPath]) {
+        [[NSFileManager defaultManager] createDirectoryAtPath:folderPath withIntermediateDirectories:NO attributes:nil error:&error]; //Create folder
+    }
+    NSString *fileName = [NSString stringWithFormat:@"KNVUNDLogFile-%@.txt", [formatter stringFromDate:date]];
+    NSString *writePath = [folderPath stringByAppendingPathComponent:fileName];
+    
+    // We're going to want to append new data, so get the previous data.
+    NSString *fileContents = [NSString stringWithContentsOfFile:writePath encoding:NSUTF8StringEncoding error:nil];
+    
+    // Write it to the string
+    string = [NSString stringWithFormat:@"%@\n%@ - %@", fileContents, [formatter stringFromDate:date], string];
+    
+    // Write to file stored at: "~/Library/Application\ Support/iPhone\ Simulator/*version*/Applications/*appGUID*/temp/KNVUNDLogFiles/KNVUNDLogFile-*date*.txt"
+    [string writeToFile:writePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+}
+
 
 #pragma mark - Getters && Setters
 static void * ShouldShowRelatedLogPropertyKey = &ShouldShowRelatedLogPropertyKey;
@@ -89,9 +131,8 @@ static void * ShouldShowRelatedLogPropertyKey = &ShouldShowRelatedLogPropertyKey
 #pragma mark - Log Related
 - (void)performConsoleLogWithLogString:(NSString *_Nonnull)string
 {
-    if ([[self class] isDevelopMode] && self.shouldShowRelatedLog) {
-        NSLog(@"%@", string);
-    }
+    [[self class] performConsoleLogWithLogString:string
+                              withControlBoolean:self.shouldShowRelatedLog];
 }
 
 - (void)performConsoleLogWithLogStringFormat:(NSString *_Nonnull)format, ... NS_FORMAT_FUNCTION(1,2)
